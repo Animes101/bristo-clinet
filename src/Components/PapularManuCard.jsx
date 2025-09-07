24e66952c0b069d1102d439332af8e013c38c389
Animes <animesbarman101@gmail.com>
2025-09-07 19:54:02 +0600

    papular card item

---
+++ src/Components/PapularManuCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PapularManuCard = ({ item }) => {
+  const { name, image,price, recipe } = item;
+
+  return (
+    <div>
+      <div className="card card-border bg-base-300 w-full p-7">
+        <img src={image} alt=""  className="w-[200px]  rounded-tr-3xl rounded-bl-3xl rounded-br-3xl"/>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>{recipe}</p>
+          <div className="card-actions justify-end">
+            <button className="">{price}$</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PapularManuCard;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactHelmet from '../Components/Layout/ReactHelmet'
 import Banner from '../Components/Banner'
 import CatagoryCard from '../Components/CatagoryCard'
 import SectionHeading from '../Components/SectionHeading'
+import PapularManuCard from '../Components/PapularManuCard'
 
 const Home = () => {
+
+  const [papular, setPapular]=useState(null)
+
+  useEffect(()=>{
+
+    fetch('Papular.json')
+    .then((res)=> res.json())
+    .then((data)=> {
+      const papularData=data.filter((items)=> items.category === 'popular')
+      setPapular(papularData)
+    })
+  },[])
+
+  console.log(papular)
   return (
-    <section>
-      <div className='container mx-auto'>
+    <section className='container mx-auto'>
+      <div>
         <Banner  />
       </div>
 
 
       {/* catagory */}
-      <div className='container  mx-auto'>
+      <div>
         <SectionHeading title="Our Categories" desc="Explore a variety of delicious dishes" time={'---From 11:00am to 10:00pm---'} />
         <CatagoryCard />
+      </div>
+
+      {/* papular manu */}
+      <SectionHeading title="FROM OUR MENU" desc="Explore a variety of delicious dishes" time={'---Check it out---'} />
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+        {papular && papular.map(item => (
+          <PapularManuCard key={item._id} item={item} />
+        ))}
       </div>
 
     </section>
